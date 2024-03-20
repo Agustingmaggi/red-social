3e842c08623892a48a52520ea7b3feebd4c5ee4a
agustin garcia maggi <agustingmaggi@gmail.com>
2024-03-20 17:00:31 +0100

    subiendo url del back al archivo app.js luego de renderizar el back

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -4,9 +4,34 @@ import Body from './components/body/body.jsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LoginForm from "./pages/IngresaPages/IngresaPage";
 import RegistrationForm from "./pages/RegistratePages/RegistratePage";
-
+import axios from "axios"
 
 function App() {
+  const url = "https://red-social-opju.onrender.com/"
+
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      axios
+        .get(url + "/data")
+        .then((el) => {
+          console.log("this is data", el)
+          setData(el.data)
+          setLoading(false)
+        })
+        .catch((err) => console.log(err))
+    }, 1000)
+    return () => clearTimeout(timeOut)
+  }, []);
+  if (loading) {
+    return (
+      <div className="App">
+        <h1>Loading please wait</h1>
+      </div>
+    )
+  }
   return (
     <Router>
       <div className="App">
